18b4b1ba0558b94dca95f2a487eb29f62abf0206
rafaelcristiano232-art <rafaelcristiano232@gmail.com>
2026-04-28 11:43:24 -0300

    Add AdminReceiver class for device admin functionality

---
+++ Android/App/Src/Main/Java/Com/Exemplo/Calculadora/AdminReceiver.java
@@ -0,0 +1,18 @@
+package com.exemplo.calculadora;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AdminReceiver extends DeviceAdminReceiver {
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        Toast.makeText(context, "Modo Administrador Ativado", Toast.LENGTH_SHORT).show();
+    }
+    
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        Toast.makeText(context, "Modo Administrador Desativado", Toast.LENGTH_SHORT).show();
+    }
+}
